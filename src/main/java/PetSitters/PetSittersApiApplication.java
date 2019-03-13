a2027e7c2c9accf5bb6612818c2c203cd6f448c9
Antoni Casas <tonicasas6@gmail.com>
2019-03-13 16:38:48 +0100

    fixing server

--- src/main/java/PetSitters/PetSittersApiApplication.java
+++ src/main/java/PetSitters/PetSittersApiApplication.java
@@ -1,9 +1,12 @@
 package PetSitters;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class PetSittersApiApplication {
 
 	public static void main(String[] args) {
