d3808d289244c25c1c15b1ba9d0e258020481684
Antoni Casas <tonicasas6@gmail.com>
2019-03-13 16:17:33 +0100

    fixing server

--- src/main/java/PetSitters/repository/UserRepository.java
+++ src/main/java/PetSitters/repository/UserRepository.java
@@ -2,11 +2,12 @@ package PetSitters.repository;
 import java.util.List;
 
 import PetSitters.entity.User;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-    public interface UserRepository extends MongoRepository<User, String> {
+@Document(collection = "domain")
+public interface UserRepository extends MongoRepository<User, String> {
 
         public User findByFirstName(String firstName);
         public List<User> findByLastName(String lastName);
